af816ff216fcc134d3aeb79c2f7cff00d1b8cf67
Michel Koekkoek <michelkoekkoek@gmail.com>
2023-05-02 22:05:54 +0200

    Add Comments

---
+++ comments/main.py
@@ -0,0 +1,28 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+voorNaam = "Piet"
+achterNaam = "de Jong"
+
+print(voorNaam) # Alleen voornaam printen
+
+# Voornaam en achternaam samenvoegen
+heleNaam = voorNaam+ " " + achterNaam
+
+# Hele naam printen
+print(heleNaam) # Voor + achternaam printen
+
+""" En nu moet ik even denken wat ik ga doen.
+Bijvoorbeeld nog een 2e persoon erbij bedenken
+Ja laten we dat doen!
+"""
+persoon2 = "Jantje Beton"
+
+""" Dan zou het nu wel leuk zijn om alles
+helemaal samen te voegen. Toch?
+Laten we ze allebei aanroepen! """
+
+print("Hoi, wat leuk dat jullie er zijn: " + heleNaam + " en " + persoon2 + "!")
+
