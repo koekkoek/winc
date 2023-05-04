c0973f63e9aabf1b7f7ce563d24594854bc6f0ab
Michel Koekkoek <michelkoekkoek@gmail.com>
2023-05-04 20:37:09 +0200

    Add Operators Excercise

---
+++ operators/main.py
@@ -0,0 +1,36 @@
+# Do not modify these lines
+__winc_id__ = 'd0d3cdcefbb54bc980f443c04ab3a9eb'
+__human_name__ = 'operators'
+
+# Add your code after this line
+
+# Language
+Language_Swiss = "German"
+Language_Spain = "Spanish"
+print(Language_Swiss == Language_Spain)
+
+# Religion
+Religion_Swiss = "Roman Catholic"
+Religion_Spain = "Roman Catholic"
+print(Religion_Swiss == Religion_Spain)
+
+# Capitals
+Capital_Swiss = len("Bern")
+Capital_Spain = len("Madrid")
+print(Capital_Spain != Capital_Swiss)
+
+# GDP
+GDP_Swiss = 618228000000000
+GDP_Spain = 1798000000000000
+print(GDP_Swiss > GDP_Spain)
+
+# Population growth
+Population_Growth_Swiss = 0.0066
+Population_Growth_Spain = 0.0067
+print(Population_Growth_Swiss < 0.01 and Population_Growth_Spain < 0.01)
+
+# Population count
+Population_Swiss = 8563760
+Population_Spain = 47222613
+print(Population_Swiss > 10000000 or Population_Spain > 10000000)
+print(Population_Swiss > 10000000 or Population_Spain > 10000000)
